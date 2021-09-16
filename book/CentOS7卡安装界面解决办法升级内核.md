7bbb16650ceed89119169855d72fd4086c4b4659
ihejia <ehejia@qq.com>
2021-09-16 19:06:48 +0800

    add centos7 update kernel

---
+++ book/CentOS7卡安装界面解决办法升级内核.md
@@ -0,0 +1,32 @@
+# CentOS7卡安装界面解决办法
+
+​	该故障原因：centos7默认内核版本较低导致在使用新硬件时安装会卡在引导界面
+
+错误提示如下：
+
+![](https://book.258tiao.com/photo/centos_install_error.jpg)
+
+```
+解决办法:
+	将该机器的硬盘安装在可以正常引导的机器安装系统后，升级内核版本为长期支持版或者最新版后，硬盘安装回该机器使用正常。
+```
+
+具体操作方法如下：
+
+### 一、选择并下载并执行
+
+##### 1、长期支持版内核
+
+```bash
+cd ~ && wget https://book.258tiao.com/script/centos7_update_kernel_lt.sh && chmod +x centos7_update_kernel_lt.sh && ./centos7_update_kernel_lt.sh && rm -rf ~/centos7_update_kernel_lt.sh
+```
+
+##### 2、最新版内核
+
+```bash
+cd ~ && wget https://book.258tiao.com/script/centos7_update_kernel_ml.sh && chmod +x centos7_update_kernel_ml.sh && ./centos7_update_kernel_ml.sh && rm -rf ~/centos7_update_kernel_ml.sh
+```
+
+### 二、重启验证内核版本
+`uname -a`
+
---
+++ script/centos7_update_kernel_ml.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#download and install
+sudo rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
+sudo yum -y install https://www.elrepo.org/elrepo-release-7.el7.elrepo.noarch.rpm
+sudo yum -y remove kernel-headers
+sudo yum -y --enablerepo=elrepo-kernel install kernel-ml.x86_64 kernel-ml-devel.x86_64 kernel-ml-headers.x86_64
+
+mbrboot()
+{
+  echo The system use mbr boot mode !
+  mbr=`sudo awk -F\' '$1=="menuentry " {print i++ " : " $2}' /etc/grub2.cfg |awk -F ": " '{print $2}' | awk NR==1`
+  echo $mbr
+  sudo grub2-set-default "$mbr"
+}
+
+uefiboot()
+{
+  echo The system uefi boot mode !
+  uefi=`sudo awk -F\' '$1=="menuentry " {print i++ " : " $2}' /boot/efi/EFI/centos/grub.cfg |awk -F ": " '{print $2}' | awk NR==1`
+  echo $uefi
+  sudo grub2-set-default "$uefi"
+}
+
+
+
+if [ -d /sys/firmware/efi ]; then
+  uefiboot
+else
+  mbrboot
+fi 
+
+#[ -d /sys/firmware/efi ] && echo UEIF || echo Leagcy
+
+echo "=================================================== "
+echo "=========  内核安装完成。系统重启后生效！  ========== "
+echo "=================================================== "
